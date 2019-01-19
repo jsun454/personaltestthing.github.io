a10decaa1d72334aeacd8e94b1822801d8e4a347
personaltestthing <46833148+personaltestthing@users.noreply.github.com>
2019-01-18 19:12:12 -0800

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -2,16 +2,11 @@ var time = Math.floor(Math.random() * 6) + 5;
 document.getElementById("countdownMessage").innerHTML = "Time until it has been " + time + " seconds since countdown began: ";
 
 function countdown() {
-	
-	
-	
-	
-	
-	
-	
+	console.log("What");
 	postMessage(time);
 	if(time > 0) {
 		time--;
+		console.log("2");
 	}
 	setTimeout("countdown()", 1000);
 }
