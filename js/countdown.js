0f147292932074145ac955c256b5e6e0af13ffaa
personaltestthing <46833148+personaltestthing@users.noreply.github.com>
2019-01-18 22:38:53 -0800

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -1,11 +1,6 @@
 var time = Math.floor(Math.random() * 6) + 5;
-var messageVisible = false;
 
 function countdown() {
-	if(!messageVisible) {
-		postMessage("Time until it has been " + time + " seconds since countdown began: ");
-		messageVisible = true;
-	}
 	postMessage(time--);
 	setTimeout("countdown()", 1000);
 }
