22b9a0cb42f032983b5b5753c3add1e4a7844534
personaltestthing <46833148+personaltestthing@users.noreply.github.com>
2019-01-18 21:21:56 -0800

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -1,17 +1,12 @@
 var time = Math.floor(Math.random() * 6) + 5;
-console.log("hid");
 var messageVisible = false;
-/*document.getElementById("countdownMessage").innerHTML = "Time until it has been " + time + " seconds since countdown began: ";
-*/
+
 function countdown() {
 	if(!messageVisible) {
-		document.getElementById("countdownMessage").innerHTML = "Time until it has been " + time + " seconds since countdown began: ";
+		postMessage("Time until it has been " + time + " seconds since countdown began: ");
 		messageVisible = true;
 	}
 	postMessage(time--);
-	if(time == 0) {
-		messageVisible = false;
-	}
 	setTimeout("countdown()", 1000);
 }
 
